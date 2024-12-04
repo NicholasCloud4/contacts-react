9d48cb0eac55abad810a47f1306071b7b3bf35c5
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-04 10:25:47 -0500

    Passing data into a component

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,17 @@
  *     - Think ahead: what's the problem with doing it this way?
  */
 
-import Contact from "./components/Contact"
+import Contact from "./components/Contact";
 
 function App() {
     return (
         <div className="contacts">
-            <Contact />
-            <Contact />
-            <Contact />
-            <Contact />
+            <Contact img="/images/mr-whiskerson.png" name="Mr. Whiskerson" phone="(212) 555-1234" email="mr.whiskaz@catnap.meow" />
+            <Contact img="/images/fluffykins.png" name="Fluffykins" phone="(212) 555-2345" email="fluff@me.com" />
+            <Contact img="/images/felix.png" name="Felix" phone="(212) 555-4567" email="felix@catnap.com" />
+            <Contact img="/images/charlie.png" name="Charlie" phone="(212) 555-6789" email="charlie@me.com" />
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Contact() {
+export default function Contact(props) {
     return (
         <article className="contact-card">
             <img
